0d31b131bbfe6307aeba8760a7ff7151cd525a48
Adam Rehman <60259367+ar819@users.noreply.github.com>
2020-11-26 19:31:32 +0000

    Update mips_control_unit.v
    Added template for opcode cases

--- rtl/mips_control_unit.v
+++ rtl/mips_control_unit.v
@@ -22,7 +22,6 @@ always_comb begin
     if (opcode == 0 && FuncCode == 6'b100001) begin //ADDU
         //SET CONTROLS
     end
-    
     else if (opcode == 6'b001001) begin //ADDIU
     end
     else if (opcode == 0 && FuncCode == 6'b100100) begin //AND
@@ -35,14 +34,90 @@ always_comb begin
     end
     else if (opcode == 6'b000001) begin //BGEZAL
     end
-    else if () begin
+    else if (opcode == 6'bxxxxxx) begin //BGTZ
     end
-    else if () begin
+    else if (opcode == 6'bxxxxxx) begin //BLEZ
     end
-    else if () begin
+    else if (opcode == 6'bxxxxxx) begin //BLTZ
     end
-    else if () begin
+    else if (opcode == 6'bxxxxxx) begin//BLTZAL
     end
-    else if (opcode == 6'bxxxxxx) begin
+    else if (opcode == 6'bxxxxxx) begin //BNE
     end
+    else if (opcode == 6'bxxxxxx) begin //DIV
+    end
+    else if (opcode == 6'bxxxxxx) begin //DIVU
+    end
+    else if (opcode == 6'bxxxxxx) begin //J
+    end
+    else if (opcode == 6'bxxxxxx) begin //JALR
+    end
+    else if (opcode == 6'bxxxxxx) begin //JAL
+    end
+    else if (opcode == 6'bxxxxxx) begin //JR
+    end
+    else if (opcode == 6'bxxxxxx) begin //LB
+    end
+    else if (opcode == 6'bxxxxxx) begin //LBU
+    end
+    else if (opcode == 6'bxxxxxx) begin //LH
+    end
+    else if (opcode == 6'bxxxxxx) begin //LHU
+    end
+    else if (opcode == 6'bxxxxxx) begin //LUI
+    end
+    else if (opcode == 6'bxxxxxx) begin //LW
+    end
+    else if (opcode == 6'bxxxxxx) begin //LWL
+    end
+    else if (opcode == 6'bxxxxxx) begin //LWR
+    end
+    else if (opcode == 6'bxxxxxx) begin //MTHI
+    end
+    else if (opcode == 6'bxxxxxx) begin //MTLO
+    end
+    else if (opcode == 6'bxxxxxx) begin //MULT
+    end
+    else if (opcode == 6'bxxxxxx) begin //MULTU
+    end
+    else if (opcode == 6'bxxxxxx) begin //OR
+    end
+    else if (opcode == 6'bxxxxxx) begin //ORI
+    end
+    else if (opcode == 6'bxxxxxx) begin //SB
+    end
+    else if (opcode == 6'bxxxxxx) begin //SH
+    end
+    else if (opcode == 6'bxxxxxx) begin //SLL
+    end
+    else if (opcode == 6'bxxxxxx) begin //SLLV
+    end
+    else if (opcode == 6'bxxxxxx) begin //SLT
+    end
+    else if (opcode == 6'bxxxxxx) begin //SLTI
+    end
+    else if (opcode == 6'bxxxxxx) begin //SLTIU
+    end
+    else if (opcode == 6'bxxxxxx) begin //SLTU
+    end
+    else if (opcode == 6'bxxxxxx) begin //SRA
+    end
+    else if (opcode == 6'bxxxxxx) begin //SRAV
+    end
+    else if (opcode == 6'bxxxxxx) begin //SRL
+    end
+    else if (opcode == 6'bxxxxxx) begin //SRLV
+    end
+    else if (opcode == 6'bxxxxxx) begin //SUBU
+    end
+    else if (opcode == 6'bxxxxxx) begin //SW
+    end
+    else if (opcode == 6'bxxxxxx) begin //XOR
+    end
+    else if (opcode == 6'bxxxxxx) begin //XORI
+    end
+    else begin
+    end
+
+
 end
