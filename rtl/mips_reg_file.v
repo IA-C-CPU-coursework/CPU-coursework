e773b2631f2ad93a4f3334b6e34cf9d09d5bdbc7
Adam Rehman <60259367+ar819@users.noreply.github.com>
2020-11-26 16:44:30 +0000

    Update mips_reg_file.v

--- rtl/mips_reg_file.v
+++ rtl/mips_reg_file.v
@@ -1,18 +1,35 @@
 module RegFile(
+    input reset;
     input CLK;
     input WriteAddress; // Which Register to write to
     input RegWrite; // Write enable
     input [31:0] DataIn; // Data to write to reg
-    input [5:0] Address1; // reg numbers
+    input [5:0] Address1; // Reg numbers
     input [5:0] Address2;
-    output [31:0] DataOut1; // output data for respective registers
+    output [31:0] DataOut1; // Output data for respective registers
     output [31:0] DataOut2;
 
 );
 
-logic [0:31][31:0] registers;
+    
+logic [0:31][31:0] registers; 
 assign registers[0] = 0; // Hardwired to 0
+    
 
+// When reset is high set all registers to zero    
+always_comb begin   
+    if (reset) begin
+        #1 if (reset) begin // Makes sure reset is high for one clock cycle
+            int i;
+            for (i=0;i<32;i++) begin
+                registers[i] = 0;
+            end
+
+        end
+    end
+end
+
+// Writing to registers
 always_ff (@posedge CLK) begin
     if(RegWrite) begin
         if(WriteAddress!=0) begin
