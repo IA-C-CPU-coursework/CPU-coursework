b81b196ede10c83621effcdded9022c3142eca00
Jason Tang <jasontang@Jasons-MacBook-Pro.local>
2020-11-27 17:13:16 +0000

    added c++ code

---
+++ rtl/mipstohex.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+string procLine(string line)
+{
+    vector<string> parts;
+    string x;
+    stringstream s(line);
+    while(s >> x){
+        parts.push_back(x);
+    }
+    if(parts.)
+}
+int main(){
+    map<string,pair<int, int>> instr;
+    instr["JR"] = make_pair(0, 8);
+    instr["LW"] = make_pair(35, 0);
+    instr["J"] = make_pair(0, 0);
+
+    string x;
+    vector<string> strings;
+    while(getline(cin, x))
+        {
+            strings.push_back(x);
+        }
+
+}
