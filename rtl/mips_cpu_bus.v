c7e6ed6115931e2301603ecab22442ffe6e3d1d7
ar819 <ar819@ic.ac.uk>
2020-11-30 08:31:04 -0800

    5 -> 3 cycle

--- rtl/mips_cpu_bus.v
+++ rtl/mips_cpu_bus.v
@@ -19,7 +19,7 @@ module mips_cpu_bus(
     wire [5:0] FuncCode = instruction [5:0];
     logic [31:0] pc;
     logic resetted = 0;
-    wire v0;
+    wire [31:0] v0;
     assign register_v0 = resetted?(active? 0:v0):0;
     initial begin
         active = 0;
@@ -34,20 +34,22 @@ module mips_cpu_bus(
     logic [31:0] instruction;
     logic [31:0] data;
 
-    typedef enum logic[3:0] {
-        FETCH_INSTR_ADDR  = 4'b0000,
-        FETCH_INSTR_DATA  = 4'b0001,
-        EXEC_INSTR_ADDR   = 4'b0010,
-        EXEC_INSTR_DATA   = 4'b0011,
-        WRITE_BACK        = 4'b0100,
-        HALTED            = 4'b0101
+    typedef enum logic[1:0] {
+        FETCH_INSTR  = 2'b00,
+        EXEC         = 2'b01,
+        WRITE_BACK   = 2'b10,
+        HALTED       = 2'b11
     } state_t;
 
     state_t state;
+
     logic [31:0] jumptarget;
+
     wire [31:0] A,B;
     wire [4:0] rd,rs,rt;
+
     wire RegDst;
+
     assign rd =  instruction [20:16];
     assign rs =  instruction[25:21];
     assign rt =  instruction[16:11];
@@ -62,7 +64,8 @@ module mips_cpu_bus(
     assign byteenable = byteenable_wire;
     wire [31:0] ALUout;
     logic resetheld = 0;
-    assign writedata = MemtoReg ? data:ALUout;
+    wire [31:0] wd;
+    assign wd = MemtoReg ? data:ALUout;
     wire [31:0] instantextended;
     wire [31:0] D2;
     assign B = ALUSrc ? instantextended:D2;
@@ -71,6 +74,7 @@ module mips_cpu_bus(
     wire jump = jumpimmediate || jumpfromreg;
     logic readinstruction;
     wire zero;
+    assign writedata = D2; 
 
     mips_sign_extension sign_extender(
         .i(instant),
@@ -98,12 +102,12 @@ module mips_cpu_bus(
     .clk(clk),
     .WriteAddress(WriteAddress),
     .RegWrite(RegWrite),
-    .DataIn(writedata),
+    .DataIn(wd),
     .Address1(rs),
     .Address2(rt),
     .DataOut1(A),
     .DataOut2(D2),
-    .regv0()
+    .regv0(v0)
     );
    
     mips_alu alu(
@@ -136,11 +140,11 @@ module mips_cpu_bus(
     logic isDelaySlot = 0;
     logic faileddelay = 0;
 
-    assign read = (state==FETCH_INSTR_ADDR) ? 1 : (state==EXEC_INSTR_ADDR && MemRead);
+    assign read = (state==FETCH_INSTR) ? 1 : (state==EXEC && MemRead);
     
-    assign write = state==EXEC_INSTR_DATA ? MemWrite : 0;
+    assign write = state==EXEC ? MemWrite : 0;
 
-    assign address  = (state == FETCH_INSTR_ADDR) ? pc:ALUout;
+    assign address  = (state == FETCH_INSTR) ? pc:ALUout;
     
 
     always @(posedge clk) begin
@@ -152,7 +156,7 @@ module mips_cpu_bus(
                 $display("CPU : Resetting.");
                 resetheld = 1;
                 pc <= 32'hBFC00000; //Reset vector 
-                state <= FETCH_INSTR_ADDR;
+                state <= FETCH_INSTR;
             end
             else begin
                 resetheld = 0;
@@ -164,92 +168,72 @@ module mips_cpu_bus(
             resetlastedge <= 0;
 
             case(state)
-                FETCH_INSTR_ADDR: begin // GETS INSTRUCTION FROM RAM
-                    
-                    $display("CPU : INFO : Fetching address: %d",pc);
-                    state <= FETCH_INSTR_ADDR;
-                   
-                end
-                FETCH_INSTR_DATA: begin //GETS REQUIRED REGISTER VALUES
+
+                /* During FETCH_INST the instruction is fetched, 
+                the control signals are determined (comb),
+                and the data is fetched from registers (comb)  */
+                FETCH_INSTR: begin 
                     $display("CPU : INFO : Fetching data from registers.");
                     if(waitrequest)begin
-                        
+                        $display("CPU : INFO : Received waitrequest while fetching instr");
                     end
                     else begin
                         instruction <= readdata;
+                        if (MemRead || MemWrite)begin
+                            state <= EXEC;
+                        end
+                        else begin
+                            state <= WRITE_BACK;
+                        end
                     end
-                    
-                   
-                    
                 end
-                EXEC_INSTR_ADDR:begin
-                    if(branchtype && !faileddelay) begin //Can only execute delay slot if not previously failed branch
+
+                /* During EXEC we handle the reading and writing of data to RAM.
+                For convenience I have made 2 cycle instructions take 3 cycles. 
+                */
+                EXEC:begin
+                    if(branchtype && !faileddelay) begin
+                        /* Here we implement a delay slot*/
                         isDelaySlot <=1;
                         pc <= pc4;
-                        state <= FETCH_INSTR_ADDR;
+                        state <= FETCH_INSTR;
                     end
                     if(isDelaySlot)begin
+                        /*Here we make sure there is no branch in delay slot and return back to prev instr if there is*/
                         if(branchtype)begin
                             $display("CPU : INFO : Attemped to branch in delay slot. Going back to previous branch.");
                             pc <= pc - 4;
-                            state <= FETCH_INSTR_ADDR;
+                            state <= FETCH_INSTR;
                             faileddelay <= 1;
                             isDelaySlot <=0;
                         end
                     end
-                    else begin
-                        state <= EXEC_INSTR_ADDR;
-                    end
-                end
 
-                
-                EXEC_INSTR_DATA: begin // MEMORY ACCESS
                     if(waitrequest) begin
-                        $display("RAM : INFO : Read data blocked by waitrequest");
+                        $display("CPU : INFO : Received waitrequest while fetching data");
                     end
                     else begin
                         data <= readdata;
-                        $display("CPU : INFO : Read data from registers");
                         state <= WRITE_BACK;
                     end
-                    
                 end
-                WRITE_BACK: begin // WRITES TO REGISTERS
 
+                WRITE_BACK: begin 
                     pc <= branchtype ? jumptarget:pc4;                    
-                    state <= FETCH_INSTR_ADDR;
+                    state <= FETCH_INSTR;
                     isDelaySlot <= 0;
-            
-                    
                 end
+
                 HALTED: begin
                     $display("CPU: WARNING : HALTED");
                     active = 0;
                 end
-
-                // FETCH_BRANCH_ADDRESS: begin
-                //     if (jump) begin 
-                //         jumptarget <= jumpfromreg ? A: instruction[25:0]; //Chooses immediate jump address or from register
-                //     end
-                //     if (branch)begin
-                        
-                //     end
-                //     pc <= pc4;
-                // end
-
-
 
                 default: begin
                     $fatal("CPU : FATAL : Unspecified state: %d",state);
                 end
             endcase
         end
-
-
-
-
-
     end 
-
 
 endmodule
