cbc74c88847cab56a96f4d74da8fd6a08fc95374
YUJIE WANG <yujie@charleswang.name>
2020-12-04 02:34:07 +0000

    🐛 fixed a bug about bit selection in mips_alu
    add 32-bit buses `HI` and `LO` on 64-bit `HILO` register to avoid bit selection inside `always_comb`

--- rtl/mips_alu.v
+++ rtl/mips_alu.v
@@ -18,6 +18,11 @@ module mips_alu(
     // 63_____31______0
     //  |__HI__|__LO__|
 
+    logic [31:0] HI;
+    logic [31:0] LO;
+    assign HI = HILO[63:32];
+    assign LO = HILO[31:0];
+
     always_comb begin
         case(ALUControl)
         // carry out required calculations based on ALUControl signal 
@@ -39,9 +44,9 @@ module mips_alu(
             5'b01110:   alu_result[31:0] = alu_src_1[31:0] -  alu_src_2[31:0]; // subtract (unsigned)
             5'b01111:   alu_result[31:0] = alu_src_1[31:0] ^  alu_src_2[31:0]; // xor
             5'b10000:   ; // in the always_ff block below                      // Move to HI
-            5'b10001:   alu_result[31:0] = HILO[63:32];                        // Move from HI
+            5'b10001:   alu_result[31:0] = HI;                                 // Move from HI
             5'b10010:   ; // in the always_ff block below                      // Move to LO
-            5'b10011:   alu_result[31:0] = HILO[31:0];                         // Move from LO
+            5'b10011:   alu_result[31:0] = LO;                                 // Move from LO
             default:    alu_result[31:0] = 32'bxxxxxxxx; 
             // output unknown signal as default behaviour
         endcase;
