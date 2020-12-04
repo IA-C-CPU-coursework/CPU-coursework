645a595a691c9850bdf0cb149a723640bab11a12
Jason Tang <jasontang@Jasons-MacBook-Pro.local>
2020-12-04 13:28:23 +0000

    added run all

--- rtl/run_one_testcase.sh
+++ rtl/run_one_testcase.sh
@@ -2,9 +2,11 @@
 set -eou pipefail
 TESTCASE="$1"
 
->&2 echo " 2 - Compiling test-bench"
+>&2 echo " 1 - Compiling test-bench"
 iverilog -g 2012 \
 -s mips_cpu_bus_tb \
--Pmips_cpu_bus_tb.RAM_INIT_FILE=\"../test/01-instruction_hex/${TESTCASE}.txt\" \
--o ../test/03_output_v0/${TESTCASE} \
+-Pmips_cpu_bus_tb.RAM_INIT_FILE=\"../test/01_instruction_hex/${TESTCASE}.txt\" \
+-o ../test/03_binaries/${TESTCASE} \
 mips_fakecpu.v mips_cpu_bus_tb_temp.v RAM_32x64k_avalon.v
+
+../test/03_binaries/${TESTCASE} > ../test/04_output_v0/${TESTCASE}.txt
